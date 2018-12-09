eebb45f39e1c2c8e086f27a641ce25643b2f79aa
Ubuntu <cs416@p2-tn1.dkmd0xwmdrhehmgf2thvhhrxzg.bx.internal.cloudapp.net>
2018-12-09 18:33:11 +0000

    run tn script

---
+++ run-tn1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ~/go/P2-q4d0b-a9h0b-i5g5-v3d0b/
+clear
+echo "clear outdated logs"
+rm *.log
+rm *.txt
+echo "Running Tor node 1"
+go run tn/main.go 10.0.0.12:8001 10.0.0.13:4001 10.0.0.13:4002 100000
